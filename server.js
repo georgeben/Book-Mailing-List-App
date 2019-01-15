a80b27e9e38094875127223abde056b7c302b8dd
georgeben <msdcconnect@gmail.com>
2019-01-15 01:06:13 +0100

    Changed PORT to use an env variable

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 require('./models/subscribers');
 const app = require('./app');
 const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
 
 //Connection to database
-console.log(process.env.DATABASE);
 mongoose.connect(process.env.DATABASE, {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 const dbConnection = mongoose.connection;
@@ -17,6 +17,6 @@ dbConnection.on('error', (err) =>{
     console.log(`Connection error ${err.message}`)
 })
 
-const server = app.listen(3000, ()=>{
+const server = app.listen(PORT, ()=>{
     console.log(`Server started on port ${server.address().port}`);
 })
